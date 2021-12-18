2a79589477f47604987ee337cced9e55601cb84b
杏梢 <73057402+hacamer@users.noreply.github.com>
2021-12-18 14:37:48 +0800

    Update clean-versions.sh

--- clean-versions.sh
+++ clean-versions.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#/system/bin/sh
 a=$(cat "rules-admin.txt" "url-filter.txt" | egrep '^! versions:' | sed -n 's/! versions://g;$p')
 b="`date +"%Y%m%d%H%M"`"
 sed -i "s/^! versions:.*/! versions:$b/g" "rules-admin.txt" "url-filter.txt"
