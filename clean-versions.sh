d7150b57d305e24aa8a961e89e6f4e4528f83470
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-01-19 10:54:41 +0800

    Update clean-versions.sh

--- clean-versions.sh
+++ clean-versions.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo '! Title: Some Rules' >> tdate.txt
 echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M') " >> tdate.txt
-echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tdate.txt
+echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）" >> tdate.txt
 cat tdate.txt rules-admin-source.txt >> rules-admin.txt
 rm -f tdate.txt
-echo '! Title: Hacamer's URL Filter' >> tdate.txt
+echo "! Title: Hacamer's URL Filter" >> tdate.txt
 echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M') " >> tdate.txt
-echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tdate.txt
+echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）" >> tdate.txt
 cat tdate.txt url-filter.txt-source.txt >> url-filter.txt.txt
 rm -f tdate.txt
 exit
