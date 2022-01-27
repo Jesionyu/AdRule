05e6d245999b0e95f6af2fcd6f207aa47635f84e
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-01-28 01:18:02 +0800

    Create main.sh

---
+++ adrules/main.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/Cats-Team/AdRules_dev/main/adguard-full.txt
+wget https://raw.githubusercontent.com/hacamer/adblock_list/master/adblock_privacy.txt
+wget https://raw.githubusercontent.com/hacamer/adblock_list/master/adblock_lite.txt
+wget -O adblock+adguard-basic-source.txt https://raw.githubusercontent.com/hacamer/adblock_list/master/adblock.txt
+wget -O adblock+adguard-full-source.txt https://raw.githubusercontent.com/hacamer/adblock_list/master/adblock_plus.txt
+
+#rm -f *.txt
+echo '! Title: AdRules (Adblock+AdGuard-basic)' >> tdate.txt
+echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M') " >> tdate.txt
+echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）" >> tdate.txt
+cat tdate.txt adblock+adguard-basic-source.txt >> adblock+adguard-basic.txt
+rm -f tdate.txt
+echo "! Title: AdRules (Adblock+AdGuard-Full)" >> tdate.txt
+echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M') " >> tdate.txt
+echo "! Last Update: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间）" >> tdate.txt
+cat tdate.txt adblock+adguard-full-source.txt >> adblock+adguard-full.txt
+rm -f tdate.txt
+exit
